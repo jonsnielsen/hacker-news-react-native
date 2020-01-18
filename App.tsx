d78b209136dc9a1b3ab00af80ae9b1713dd6b648
Jonathan Sparvath <jonathan.sparvath@gmail.com>
2020-01-18 11:48:41 +0100

    first commit

---
+++ App.tsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+
+export default function App() {
+  const [outputText, setOutputText] = useState(
+    "Open up App.tsx to start working on your app!"
+  );
+  return (
+    <View style={styles.container}>
+      <Text>{outputText}</Text>
+      <Button
+        title="Change Text"
+        onPress={() => {
+          setOutputText("New text");
+        }}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
